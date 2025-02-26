77871b972bdea58cf88aff85cee31efcab6733b6
Evan Stoner <evan.stoner@crowdstrike.com>
2025-02-26 15:13:55 -0500

    chore: bump rollup and terser plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,11 @@
 import { babel } from "@rollup/plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import postcss from "rollup-plugin-postcss";
-import { terser } from "rollup-plugin-terser";
-import pkg from "./package.json";
+import pkg from "./package.json" assert { type: "json" };
 
 export default {
   input: "src/index.ts",
